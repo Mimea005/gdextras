b338e1d77f4ea0fa2cece0bd69e0f8438ad8aec1
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-06-13 15:20:05 +0200

    Made Interpolation trait public and added documentation

--- src/math.rs
+++ src/math.rs
@@ -1,15 +1,18 @@
 use std::ops::Range;
 use num_traits::Float;
 
-
-trait Interpolation
+/// Interpolate between two floats
+pub trait Interpolation
     where Self: Float
 {
 
+    /// Linear interpolation between self and b by t amount
     fn lerp(self, b: Self, t: Self) -> Self;
 
+    /// Get the interpolation amount between a and b
     fn lerp_inv(self, a: Self, b: Self) -> Self;
 
+    /// remap self from range i to range o
     fn remap(self, i: Range<Self>, o: Range<Self>) -> Self {
         let t = self.lerp_inv(i.start, i.end);
         o.start.lerp(o.end, t)
